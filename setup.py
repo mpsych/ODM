767519c5e6d283364a5f48df80a98e28e6aecb7c
RyanZurrin <ryanzurrin@ryanzurrin.com>
2023-07-22 16:06:10 -0400

    trying to fix the setup.py.

--- setup.py
+++ setup.py
@@ -2,19 +2,18 @@ from setuptools import setup, find_packages
 
 from odm import __version__
 
+with open('README.md', 'r', encoding='utf-8') as fh:
+    long_description = fh.read()
+
 setup(
     name="ODMammogram",
     version=__version__,
     url="https://github.com/RyanZurrin/ODMammogram",
     author="Ryan Zurrin, Daniel Haehn",
-    author_email="ryan.zurrin001@umb.edu, daniel.haehn@umb.edu",
-    description="Outlier Detection for Mammograms (ODM) is a Python package for medical "
-    "image analysis with a focus on detection of outliers in DICOM mammogram "
-    "images. Leveraging techniques such as 5-bin histogram thresholding and "
-    "variational auto-encoders, ODM facilitates automated identification of "
-    "images with atypical characteristics. Its user-friendly API allows seamless "
-    "integration with existing pipelines, making it a handy tool for large-scale "
-    "studies and quality assurance in mammography medical imaging research.",
+    author_email="Ryan Zurrin <ryan.zurrin001@umb.edu>, Daniel Haehn <daniel.haehn@umb.edu>",
+    description="Outlier Detection for Mammograms (ODM) is a Python package for medical image analysis...",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     packages=find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3.9",
